6a848e72ab02f63b0a444db7e36946dd51597622
sajit94 <sajith.sajith44@gmail.com>
2026-08-03 13:04:48 +0530

    8

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,11 +1,11 @@
 variable "tenancy_ocid" { type = string }
 variable "user_ocid" { type = string }
 variable "fingerprint" { type = string }
-variable "private_key_path" { type = string }
+variable "private_key" { type = string } # <--- This must be private_key
 variable "region" { type = string }
 variable "compartment_ocid" { type = string }
 variable "ssh_public_key" { type = string }
 variable "bucket_name" {
   type    = string
-  default = "goa-trip-memories-bucket"
+  default = "goa-trip-memories-sajith-1"
 }
